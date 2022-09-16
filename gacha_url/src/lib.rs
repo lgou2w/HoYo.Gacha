4d43b173e16190470335b93ea8e075bce139eca6
lgou2w <lgou2w@hotmail.com>
2022-09-16 21:06:18 +0800

    feat: refactor with multiple modules

--- gacha_url/src/lib.rs
+++ gacha_url/src/lib.rs
@@ -3,28 +3,34 @@ extern crate form_urlencoded;
 
 use std::collections::HashMap;
 use std::io::{Error, ErrorKind, Result};
-use chrono::{DateTime, Utc};
-use crate::disk_cache;
-use crate::genshin;
+use chrono::{DateTime, NaiveDateTime, Utc};
+use disk_cache::index_file::IndexFile;
+use disk_cache::block_file::BlockFile;
+use disk_cache::entry_store::EntryStore;
+use genshin_path;
 
 const GACHA_URL_ENDPOINT: &'static str = "/event/gacha_info/api/getGachaLog?";
 const GACHA_URL_ENDPOINT_LEN: usize = GACHA_URL_ENDPOINT.len();
 
 pub fn find_recent_gacha_url() -> Result<(DateTime<Utc>, String)> {
-  let genshin_data_dir = genshin::path::get_data_dir_path()?;
+  let genshin_data_dir = genshin_path::get_game_data_dir_path()?;
   let cache_dir = genshin_data_dir.join("webCaches/Cache/Cache_Data");
-  let disk_cache = disk_cache::structs::DiskCache::from_dir(cache_dir)?;
+
+  let index_file = IndexFile::from_file(cache_dir.join("index"))?;
+  let block_file1 = BlockFile::from_file(cache_dir.join("data_1"))?;
+  let block_file2 = BlockFile::from_file(cache_dir.join("data_2"))?;
 
   let mut records: Vec<(DateTime<Utc>, String)> = Vec::new();
-  for addr in disk_cache.index_file.table.iter().cloned() {
-    let entry = disk_cache.read_entry(addr)?;
-    if entry.long_key == 0 {
+  for addr in index_file.table.iter().cloned() {
+
+    let entry = EntryStore::from_block_file(&block_file1, addr)?;
+    if !entry.is_long_url() {
       // Gacha url must be a long key and stored in the data_2 block file,
       // So the long key of entry must not be zero.
       continue;
     }
 
-    let mut url = disk_cache.read_entry_key_as_url(&entry)?;
+    let mut url = entry.get_long_url(&block_file2)?;
 
     // Get only valid gacha url
     if !url.contains(GACHA_URL_ENDPOINT) {
@@ -36,7 +42,7 @@ pub fn find_recent_gacha_url() -> Result<(DateTime<Utc>, String)> {
       url = url[4..].to_string();
     }
 
-    let creation_time = entry.get_creation_time_as_utc();
+    let creation_time = entry_store_creation_time_as_utc(&entry);
     records.push((creation_time, url));
   }
 
@@ -54,4 +60,22 @@ pub fn parse_gacha_url_queries(gacha_url: &String) -> HashMap<String, String> {
   let start = gacha_url.find(GACHA_URL_ENDPOINT).expect("Invalid gacha url");
   let query = &gacha_url[start + GACHA_URL_ENDPOINT_LEN..];
   form_urlencoded::parse(query.as_bytes()).into_owned().collect()
+}
+
+fn windows_ticks_to_unix_timestamps(ticks: u64) -> (i64, u32) {
+  let seconds = ticks / 10_000_000 - 11_644_473_600;
+  let nano_seconds = ticks % 10_000_000;
+  (seconds as i64, nano_seconds as u32)
+}
+
+fn entry_store_creation_time_as_utc(entry: &EntryStore) -> DateTime<Utc> {
+  let creation_time = entry.creation_time;
+  let (seconds, nano_seconds) = windows_ticks_to_unix_timestamps(
+    // The creation time of the entry store must be multiplied by 10 for correct windows ticks
+    creation_time * 10
+  );
+  NaiveDateTime
+    ::from_timestamp(seconds, nano_seconds)
+    .and_local_timezone(Utc)
+    .unwrap()
 }
