89a76b480f037319ce09bd4c971bedc3daaef5d7
lgou2w <lgou2w@hotmail.com>
2022-09-18 20:19:51 +0800

    feat(wip): export excel gacha logs in cli tool

--- gacha_cli/src/main.rs
+++ gacha_cli/src/main.rs
@@ -1,8 +1,10 @@
 extern crate clap;
 extern crate chrono;
+extern crate serde_json;
 extern crate tokio;
 
-use std::io::ErrorKind;
+use std::fs::File;
+use std::io::{BufWriter, ErrorKind, Write};
 use std::path::PathBuf;
 use clap::{arg, value_parser, ArgAction, ArgMatches, Command};
 use chrono::{Duration, Local, Utc, SecondsFormat, DateTime};
@@ -24,7 +26,6 @@ fn cli() -> Command<'static> {
     .subcommand(
       Command::new("logs")
         .about("Fetch all gacha logs from gacha url")
-        .arg(arg!(-t --type [TYPE] "Specify gacha type. [100, 200, 301, 400, 302]"))
         .arg(arg!(-f --format <FORMAT> "Set output format. (json, excel)"))
         .arg(arg!(-o --out <FILE> "Set output file.").value_parser(value_parser!(PathBuf)))
     )
@@ -72,10 +73,10 @@ fn get_genshin_gacha_url(sub_matches: &ArgMatches) {
 }
 
 async fn get_genshin_gacha_logs(sub_matches: &ArgMatches) {
-  let types = sub_matches.get_many::<String>("type").unwrap();
+  let format = sub_matches.get_one::<String>("format").unwrap();
 
-  // sub_matches.get_one::<String>("format");
-  // sub_matches.get_one::<PathBuf>("out");
+  let out_path = sub_matches.get_one::<PathBuf>("out").unwrap();
+  let mut out_file = File::create(out_path).unwrap();
 
   let genshin_data_dir = genshin_path::get_game_data_dir_path().unwrap();
   let (creation_time, gacha_url) = find_gacha_url(genshin_data_dir);
@@ -86,10 +87,20 @@ async fn get_genshin_gacha_logs(sub_matches: &ArgMatches) {
     panic!("Last gacha url has expired. Please reopen the gacha history page in the game!")
   }
 
-  let gacha_logs = gacha_log::fetch_gacha_logs(&gacha_url, "200").await;
-  for log in gacha_logs {
-    println!("{:?}", log);
+  println!("Fetch gacha logs...");
+
+  let gacha_types = vec![(301, "角色活动祈愿"), (302, "武器活动祈愿"), (200, "常驻祈愿"), (100, "新手祈愿")];
+  let mut gacha_logs_vec = Vec::new();
+  for (gacha_type, name) in gacha_types.iter() {
+    println!("Fetch gacha type: {} ({})", gacha_type, *name);
+    let gacha_logs = gacha_log::fetch_gacha_logs(&gacha_url, &gacha_type.to_string()).await;
+    gacha_logs_vec.push((*name, gacha_logs));
   }
 
-  // TODO: Excel - UIGF.W, Json - UIGF.J
+
+  println!("Writing...");
+
+  let excel_gacha_log = gacha_log::convert_to_excel(gacha_logs_vec);
+  out_file.write(&excel_gacha_log).unwrap();
+
 }
