cdbfd7557ff5494b7aa9070fd181d48614e0cb2c
lgou2w <lgou2w@hotmail.com>
2023-01-16 20:42:53 +0800

    feat: add eslint and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function App() {
+export default function App () {
   return (
-    <div className="app">
-      <h1>Genshin Gacha</h1>
+    <div className="app m-4">
+      <h1 className="text-xl text-red-300">Genshin Gacha</h1>
+      <p className="text-lg text-black">原神</p>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './assets/css/tailwind.css'
+import './assets/css/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
