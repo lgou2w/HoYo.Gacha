6ebae494e6a4d2aaf0bd976d2518148164c2742f
lgou2w <lgou2w@hotmail.com>
2023-02-03 17:39:11 +0800

    chore: add react-devtools script plugin to vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,34 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    // Prepend the script if it is not a production environment and the React developer tools are enabled
+    // See:
+    //   https://github.com/facebook/react/tree/main/packages/react-devtools#usage-with-react-dom
+    //   https://vitejs.dev/guide/api-plugin.html#transformindexhtml
+    {
+      name: 'react-devtools-script-plugin',
+      transformIndexHtml () {
+        // Example usage:
+        //   Default url:
+        //     REACT_DEVTOOLS=1 pnpm tauri dev
+        //   Custom url:
+        //     REACT_DEVTOOLS_URL=http://localhost:4567 pnpm tauri dev
+        //
+        if (process.env.NODE_ENV !== 'production' &&
+          (process.env.REACT_DEVTOOLS === '1' ||
+          typeof process.env.REACT_DEVTOOLS_URL !== 'undefined')
+        ) {
+          return [{
+            injectTo: 'head-prepend',
+            tag: 'script',
+            attrs: {
+              src: process.env.REACT_DEVTOOLS_URL || 'http://localhost:8097'
+            }
+          }]
+        }
+      }
+    }
   ],
   clearScreen: false,
   server: {
