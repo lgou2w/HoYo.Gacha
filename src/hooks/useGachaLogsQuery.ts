1b2f633a5fbcda2d2edc61b6a6ff053fdafee2ab
lgou2w <lgou2w@hotmail.com>
2023-03-16 14:48:42 +0800

    fix: aggregated metadata is not calculated correctly

--- src/hooks/useGachaLogsQuery.ts
+++ src/hooks/useGachaLogsQuery.ts
@@ -75,14 +75,19 @@ function computeGrouped (data: GachaLogItem[]): GroupedGachaLogs {
     return acc
   }, valuesAcc)
 
+  const newbie = computeGroupedNamed('newbie', Array.from(values[100]))
+  const permanent = computeGroupedNamed('permanent', Array.from(values[200]))
+  const character = computeGroupedNamed('character', Array.from(values[301])
+    .concat(Array.from(values[400]))
+    .sort(sortGachaLogItemById))
+  const weapon = computeGroupedNamed('weapon', Array.from(values[302]))
+  const aggregated = computeGroupedNamedAggregated(data, newbie, permanent, character, weapon)
   const namedValues: GroupedGachaLogs['namedValues'] = {
-    newbie: computeGroupedNamed('newbie', Array.from(values[100])),
-    permanent: computeGroupedNamed('permanent', Array.from(values[200])),
-    character: computeGroupedNamed('character', Array.from(values[301])
-      .concat(values[400])
-      .sort((a, b) => a.id.localeCompare(b.id))),
-    weapon: computeGroupedNamed('weapon', Array.from(values[302])),
-    aggregated: computeGroupedNamed('aggregated', data)
+    newbie,
+    permanent,
+    character,
+    weapon,
+    aggregated
   }
 
   const lastCharacterId = values[301][values[301].length - 1]?.id as string | undefined
@@ -138,6 +143,74 @@ function computeGroupedNamed (category: NamedGachaLogsCategory, data: GachaLogIt
   }
 }
 
+function computeGroupedNamedAggregated (
+  data: GachaLogItem[],
+  newbie: NamedGachaLogs,
+  permanent: NamedGachaLogs,
+  character: NamedGachaLogs,
+  weapon: NamedGachaLogs
+): NamedGachaLogs {
+  const total = data.length
+
+  const blueSum = newbie.metadata.blue.sum + permanent.metadata.blue.sum + character.metadata.blue.sum + weapon.metadata.blue.sum
+  const blueSumPercentage = blueSum > 0 ? Math.round((blueSum / total) * 10000) / 100 : 0
+  const blueValues = data.filter(item => item.rankType === '3')
+
+  const purpleSum = newbie.metadata.purple.sum + permanent.metadata.purple.sum + character.metadata.purple.sum + weapon.metadata.purple.sum
+  const purpleSumPercentage = purpleSum > 0 ? Math.round((purpleSum / total) * 10000) / 100 : 0
+  const purpleValues = data.filter(item => item.rankType === '4')
+
+  const goldenSum = newbie.metadata.golden.sum + permanent.metadata.golden.sum + character.metadata.golden.sum + weapon.metadata.golden.sum
+  const goldenSumPercentage = goldenSum > 0 ? Math.round((goldenSum / total) * 10000) / 100 : 0
+  const goldenValues = Array.from(newbie.metadata.golden.values)
+    .concat(Array.from(permanent.metadata.golden.values))
+    .concat(Array.from(character.metadata.golden.values))
+    .concat(Array.from(weapon.metadata.golden.values))
+    .sort(sortGachaLogItemById)
+
+  const { goldenSumRestricted, goldenUsedPitySum } = goldenValues.reduce((acc, item) => {
+    if (item.restricted) {
+      acc.goldenSumRestricted += 1
+    }
+    acc.goldenUsedPitySum += item.usedPity
+    return acc
+  }, {
+    goldenSumRestricted: 0,
+    goldenUsedPitySum: 0
+  })
+
+  const goldenSumAverage = goldenSum > 0 ? Math.ceil(Math.round((goldenUsedPitySum / goldenSum) * 100) / 100) : 0
+
+  return {
+    category: 'aggregated',
+    categoryTitle: CategoryTitles.aggregated,
+    total,
+    firstTime: data[0]?.time,
+    lastTime: data[data.length - 1]?.time,
+    values: data,
+    metadata: {
+      blue: {
+        sum: blueSum,
+        sumPercentage: blueSumPercentage,
+        values: blueValues
+      },
+      purple: {
+        sum: purpleSum,
+        sumPercentage: purpleSumPercentage,
+        values: purpleValues
+      },
+      golden: {
+        sum: goldenSum,
+        sumPercentage: goldenSumPercentage,
+        values: goldenValues,
+        sumAverage: goldenSumAverage,
+        sumRestricted: goldenSumRestricted,
+        nextPity: 0 // aggregated not needed
+      }
+    }
+  }
+}
+
 function computeGachaLogsMetadata (total: number, ranked: GachaLogItem[]): GachaLogsMetadata {
   const sum = ranked.length
   const sumPercentage = sum > 0 ? Math.round((sum / total) * 10000) / 100 : 0
@@ -181,4 +254,8 @@ function computeGoldenGachaLogsMetadata (data: GachaLogItem[]): GoldenGachaLogsM
     sumRestricted,
     nextPity: pity
   }
+}
+
+function sortGachaLogItemById (a: GachaLogItem, b: GachaLogItem): number {
+  return a.id.localeCompare(b.id)
 }
