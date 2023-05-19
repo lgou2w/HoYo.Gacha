0cfd75327907145511423940a0e58394e5a0e9e0
lgou2w <lgou2w@hotmail.com>
2023-05-19 19:34:33 +0800

    fix: the get_version command has no date

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -11,14 +11,15 @@ pub fn get_handlers<R: Runtime>() -> Box<dyn Fn(Invoke<R>) + Send + Sync> {
 
 #[tauri::command]
 fn get_version() -> String {
+  let date = constants::COMMIT_DATE.split('T').collect::<Vec<&str>>()[0];
   let commit_tag = constants::COMMIT_TAG;
   if !commit_tag.is_empty() && commit_tag.to_lowercase().contains("snapshot") {
-    return commit_tag.to_owned()
+    format!("{} ({})", commit_tag, date)
+  } else {
+    format!("{}-{} ({})",
+      constants::VERSION,
+      constants::COMMIT_HASH[0..7].to_owned(),
+      date
+    )
   }
-
-  format!("{}-{} ({})",
-    constants::VERSION,
-    constants::COMMIT_HASH[0..7].to_owned(),
-    constants::COMMIT_DATE.split('T').collect::<Vec<&str>>()[0]
-  )
 }
