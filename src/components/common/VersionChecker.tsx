7b97d362c46866faff5740c74147897dc55f356b
lgou2w <lgou2w@hotmail.com>
2023-06-09 10:40:02 +0800

    chore: compare version check

--- src/components/common/VersionChecker.tsx
+++ src/components/common/VersionChecker.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { dialog, process } from '@tauri-apps/api'
-import { useVersion, useLatestVersion } from '@/components/common/useVersion'
+import { useVersion, useLatestVersion, CurrentVersion, LatestVersion } from '@/components/common/useVersion'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import DownloadIcon from '@mui/icons-material/Download'
@@ -53,8 +53,7 @@ export default function VersionChecker () {
   if (latestVersion.isLoading) return <Typography component="span" variant="body2">加载中...</Typography>
   if (latestVersion.isError) return <Typography component="span" variant="body2" color="error">检查最新版本失败</Typography>
 
-  // TODO: semver
-  const needUpdate = dayjs(version.data.date).isBefore(latestVersion.data.created_at)
+  const needUpdate = isNeedUpdate(version.data, latestVersion.data)
   if (!needUpdate) return <Typography component="span" variant="body2" color="error">已是最新版本</Typography>
 
   return (
@@ -66,4 +65,22 @@ export default function VersionChecker () {
       {!busy ? '更新至新版本' : '下载最新版本中...'}
     </Button>
   )
+}
+
+function isNeedUpdate (
+  version: CurrentVersion,
+  latestVersion: LatestVersion | null
+): boolean {
+  if (!latestVersion) return false
+
+  // TODO: Strictly compare semver
+  const current = version.version
+  const latest = latestVersion.tag_name
+  const currentSemver = current.split('.').map(v => parseInt(v))
+  const latestSemver = latest.split('.').map(v => parseInt(v))
+  for (let i = 0; i < 3; i++) {
+    if (currentSemver[i] < latestSemver[i]) return true
+    if (currentSemver[i] > latestSemver[i]) return false
+  }
+  return false
 }
