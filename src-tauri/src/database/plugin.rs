64368a26e1c78a28819c4a57c6d63e3915375ab2
lgou2w <lgou2w@hotmail.com>
2023-11-03 11:07:52 +0800

    feat: handlers macro for database plugin

--- src-tauri/src/database/plugin.rs
+++ src-tauri/src/database/plugin.rs
@@ -25,138 +25,92 @@ impl DatabasePluginBuilder {
         Ok(())
       })
       .invoke_handler(generate_handler![
-        handlers::find_accounts,
-        handlers::find_accounts_by_facet,
-        handlers::find_account_by_id,
-        handlers::find_account_by_facet_and_uid,
-        handlers::create_account,
-        handlers::delete_account,
-        handlers::update_account_game_data_dir,
-        handlers::update_account_gacha_url,
-        handlers::update_account_properties
+        handler::find_accounts,
+        handler::find_accounts_by_facet,
+        handler::find_account_by_id,
+        handler::find_account_by_facet_and_uid,
+        handler::create_account,
+        handler::delete_account,
+        handler::update_account_game_data_dir,
+        handler::update_account_gacha_url,
+        handler::update_account_properties
       ])
       .build()
   }
 }
 
 // Handlers
-//   TODO: macro_rules
 
-mod handlers {
+macro_rules! generate_handlers {
+  ($questioner:ident, {
+    $(
+      $name:ident {
+        $($arg_n:ident: $arg_t:ty),*
+      } $query:ident and $operation:ident => $result:ty,
+    )*
+  }) => {
+    $(
+      #[tauri::command]
+      pub async fn $name(
+        database: DatabasePluginState<'_>,
+        $($arg_n: $arg_t),*
+      ) -> Result<$result, String> {
+        $questioner
+          .$query($($arg_n),*)
+          .$operation(database.executor())
+          .await
+          .map_err(|e| format!("{e}")) // TODO: error
+      }
+    )*
+  };
+}
+
+mod handler {
   use time::OffsetDateTime;
 
   use super::DatabasePluginState;
   use crate::database::{Account, AccountFacet, AccountProperties, AccountQuestioner};
 
-  #[tauri::command]
-  pub async fn find_accounts(database: DatabasePluginState<'_>) -> Result<Vec<Account>, String> {
-    AccountQuestioner
-      .find_many()
-      .fetch_all(database.executor())
-      .await
-      .map_err(|e| format!("{e}")) // TODO: error
-  }
+  generate_handlers!(AccountQuestioner, {
+    find_accounts {} find_many and fetch_all => Vec<Account>,
 
-  #[tauri::command]
-  pub async fn find_accounts_by_facet(
-    database: DatabasePluginState<'_>,
-    facet: AccountFacet,
-  ) -> Result<Vec<Account>, String> {
-    AccountQuestioner
-      .find_many_by_facet(facet)
-      .fetch_all(database.executor())
-      .await
-      .map_err(|e| format!("{e}")) // TODO: error
-  }
+    find_accounts_by_facet {
+      facet: AccountFacet
+    } find_many_by_facet and fetch_all => Vec<Account>,
 
-  #[tauri::command]
-  pub async fn find_account_by_id(
-    database: DatabasePluginState<'_>,
-    id: u32,
-  ) -> Result<Option<Account>, String> {
-    AccountQuestioner
-      .find_one_by_id(id)
-      .fetch_optional(database.executor())
-      .await
-      .map_err(|e| format!("{e}")) // TODO: error
-  }
+    find_account_by_id {
+      id: u32
+    } find_one_by_id and fetch_optional => Option<Account>,
 
-  #[tauri::command]
-  pub async fn find_account_by_facet_and_uid(
-    database: DatabasePluginState<'_>,
-    facet: AccountFacet,
-    uid: u32,
-  ) -> Result<Option<Account>, String> {
-    AccountQuestioner
-      .find_one_by_facet_and_uid(facet, uid)
-      .fetch_optional(database.executor())
-      .await
-      .map_err(|e| format!("{e}")) // TODO: error
-  }
+    find_account_by_facet_and_uid {
+      facet: AccountFacet,
+      uid: u32
+    } find_one_by_facet_and_uid and fetch_optional => Option<Account>,
 
-  #[tauri::command]
-  pub async fn create_account(
-    database: DatabasePluginState<'_>,
-    facet: AccountFacet,
-    uid: u32,
-    game_data_dir: String,
-  ) -> Result<Account, String> {
-    AccountQuestioner
-      .create_one(facet, uid, game_data_dir)
-      .fetch_one(database.executor())
-      .await
-      .map_err(|e| format!("{e}")) // TODO: error
-  }
+    create_account {
+      facet: AccountFacet,
+      uid: u32,
+      game_data_dir: String
+    } create_one and fetch_one => Account,
 
-  #[tauri::command]
-  pub async fn delete_account(
-    database: DatabasePluginState<'_>,
-    id: u32,
-  ) -> Result<Option<Account>, String> {
-    AccountQuestioner
-      .delete_by_id(id)
-      .fetch_optional(database.executor())
-      .await
-      .map_err(|e| format!("{e}")) // TODO: error
-  }
+    delete_account {
+      id: u32
+    } delete_by_id and fetch_optional => Option<Account>,
 
-  #[tauri::command]
-  pub async fn update_account_game_data_dir(
-    database: DatabasePluginState<'_>,
-    game_data_dir: String,
-    id: u32,
-  ) -> Result<Option<Account>, String> {
-    AccountQuestioner
-      .update_game_data_dir_by_id(game_data_dir, id)
-      .fetch_optional(database.executor())
-      .await
-      .map_err(|e| format!("{e}")) // TODO: error
-  }
+    update_account_game_data_dir {
+      game_data_dir: String,
+      id: u32
+    } update_game_data_dir_by_id and fetch_optional => Option<Account>,
 
-  #[tauri::command]
-  pub async fn update_account_gacha_url(
-    database: DatabasePluginState<'_>,
-    gacha_url: Option<String>,
-    gacha_url_updated_at: Option<OffsetDateTime>,
-    id: u32,
-  ) -> Result<Option<Account>, String> {
-    AccountQuestioner
-      .update_gacha_url_by_id(gacha_url, gacha_url_updated_at, id)
-      .fetch_optional(database.executor())
-      .await
-      .map_err(|e| format!("{e}")) // TODO: error
-  }
+    update_account_gacha_url {
+      gacha_url: Option<String>,
+      gacha_url_updated_at: Option<OffsetDateTime>,
+      id: u32
+    } update_gacha_url_by_id and fetch_optional => Option<Account>,
 
-  #[tauri::command]
-  pub async fn update_account_properties(
-    database: DatabasePluginState<'_>,
-    properties: Option<AccountProperties>,
-    id: u32,
-  ) -> Result<Option<Account>, String> {
-    AccountQuestioner
-      .update_properties_by_id(properties, id)
-      .fetch_optional(database.executor())
-      .await
-      .map_err(|e| format!("{e}")) // TODO: error
-  }
+    update_account_properties {
+      properties: Option<AccountProperties>,
+      id: u32
+    } update_properties_by_id and fetch_optional => Option<Account>,
+  });
 }
