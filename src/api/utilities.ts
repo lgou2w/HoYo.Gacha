0ee3bff0f495871ae2f1959804ceea9a352aa6db
lgou2w <lgou2w@hotmail.com>
2024-06-28 13:09:38 +0800

    chore: update utilities.ts

--- src/api/utilities.ts
+++ src/api/utilities.ts
@@ -1,3 +1,12 @@
-export function capitalize (str: string): string {
-  return str.charAt(0).toUpperCase() + str.slice(1)
+export function capitalize (str: null): null
+export function capitalize (str: undefined): undefined
+export function capitalize (str: string): string
+export function capitalize (str: string | null | undefined): string | undefined | null {
+  if (str === null) {
+    return null
+  } else if (typeof str === 'undefined') {
+    return undefined
+  } else if (typeof str === 'string') {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
 }
