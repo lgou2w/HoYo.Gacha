2f93130b558c0a7ff3b31f06e59ec9c3efb09c9c
lgou2w <lgou2w@hotmail.com>
2025-06-27 16:33:50 +0800

    chore: update build artifact script

--- .scripts/build-artifact.cjs
+++ .scripts/build-artifact.cjs
@@ -1,3 +1,5 @@
+'use strict'
+
 const fs = require('node:fs')
 const path = require('node:path')
 const child_process = require('node:child_process')
@@ -8,19 +10,19 @@ const packageJson = require('../package.json')
 const mainBinaryName = 'HoYo_Gacha'
 const isWindows = process.platform === 'win32'
 const executable = isWindows ? 'exe' : undefined
-
 const cwd = path.resolve(__dirname, '..')
-const gitHash = child_process
-  .execSync('git rev-parse --short HEAD', { cwd })
-  .toString('utf-8')
-  .trim()
 
 const buildDir = path.join(cwd, 'target', 'release')
 const buildFile = executable && path.join(buildDir, `${mainBinaryName}.${executable}`)
 if (!buildFile || !fs.existsSync(buildFile)) {
-  console.error(`Build file not found: ${buildFile}`);
-  process.exit(1);
+  console.error(`Build file not found: ${buildFile}`)
+  process.exit(1)
 }
+
+const gitHash = child_process
+  .execSync('git rev-parse --short HEAD', { cwd })
+  .toString('utf-8')
+  .trim()
 
 const distFilename = packageJson.displayName + '.' + packageJson.version + '.git-' + gitHash + '.' + executable
 const distFile = path.join(buildDir, distFilename)
@@ -31,10 +33,11 @@ const hasher = crypto.createHash('sha256')
 fs.createReadStream(distFile)
   .pipe(hasher)
   .once('error', (error) => {
-    console.error('Error hashing file:', error);
-    process.exit(1);
+    console.error('Error hashing file:', error)
+    process.exit(1)
   })
   .once('finish', () => {
     const hash = hasher.read().toString('hex')
     fs.writeFileSync(hashFile, hash)
+    console.log(distFile)
   })
