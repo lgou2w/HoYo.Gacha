1fea5c00cb7436558f929b5e7bf011e3512c0c9f
lgou2w <lgou2w@hotmail.com>
2025-07-11 06:30:55 +0800

    chore: update chart placeholder text

--- src/pages/Gacha/LegacyView/Clientarea/Chart/index.tsx
+++ src/pages/Gacha/LegacyView/Clientarea/Chart/index.tsx
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 export default function GachaLegacyViewClientareaChart () {
   return (
     <Fragment>
-      Chart
+      Coming soon...
     </Fragment>
   )
 }
