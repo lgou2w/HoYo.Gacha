9d5f9c08a8f2aefb4d44ec038c15d84f9302eabc
lgou2w <lgou2w@hotmail.com>
2025-07-15 02:51:32 +0800

    chore: update build script and workflow

--- .scripts/build-artifact.cjs
+++ .scripts/build-artifact.cjs
@@ -2,8 +2,6 @@
 
 const fs = require('node:fs')
 const path = require('node:path')
-const child_process = require('node:child_process')
-const crypto = require('node:crypto')
 const packageJson = require('../package.json')
 
 const isWindows = process.platform === 'win32'
@@ -17,25 +15,7 @@ if (!buildFile || !fs.existsSync(buildFile)) {
   process.exit(1)
 }
 
-const gitHash = child_process
-  .execSync('git rev-parse --short HEAD', { cwd })
-  .toString('utf-8')
-  .trim()
-
-const distFilename = packageJson.displayName + '.' + packageJson.version + '.git-' + gitHash + '.' + executable
+const distFilename = packageJson.displayName + '.' + executable
 const distFile = path.join(buildDir, distFilename)
 fs.copyFileSync(buildFile, distFile)
-
-const hashFile = path.join(buildDir, distFilename + '.sha256')
-const hasher = crypto.createHash('sha256')
-fs.createReadStream(distFile)
-  .pipe(hasher)
-  .once('error', (error) => {
-    console.error('Error hashing file:', error)
-    process.exit(1)
-  })
-  .once('finish', () => {
-    const hash = hasher.read().toString('hex')
-    fs.writeFileSync(hashFile, hash)
-    console.log(distFile)
-  })
+console.log(distFile)
