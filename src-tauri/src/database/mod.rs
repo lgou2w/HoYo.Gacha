f02167837f12d4bc9188aaf77b7c43d33fda7e8e
lgou2w <lgou2w@hotmail.com>
2025-07-19 21:43:01 +0800

    fix(#74): adjust the primary key of the gacha records

--- src-tauri/src/database/mod.rs
+++ src-tauri/src/database/mod.rs
@@ -200,7 +200,58 @@ PRAGMA USER_VERSION = 1;
 COMMIT TRANSACTION;
 ";
 
-const SQLS: &[&str] = &[SQL_V1];
+// Changes:
+// Table: `HG_GACHA_RECORDS`
+// PK   : (business, uid, id)
+//      : (business, uid, id, gacha_type)
+//
+// See  : https://github.com/lgou2w/HoYo.Gacha/issues/74
+
+const SQL_V2: &str = r"
+BEGIN TRANSACTION;
+SAVEPOINT start_migration_v2;
+
+ALTER TABLE `HG_GACHA_RECORDS` RENAME TO `HG_GACHA_RECORDS_OLD`;
+
+CREATE TABLE IF NOT EXISTS `HG_GACHA_RECORDS` (
+  `business`   INTEGER NOT NULL,
+  `uid`        INTEGER NOT NULL,
+  `id`         TEXT    NOT NULL,
+  `gacha_type` INTEGER NOT NULL,
+  `gacha_id`   INTEGER,
+  `rank_type`  INTEGER NOT NULL,
+  `count`      INTEGER NOT NULL,
+  `time`       TEXT    NOT NULL,
+  `lang`       TEXT    NOT NULL,
+  `name`       TEXT    NOT NULL,
+  `item_type`  TEXT    NOT NULL,
+  `item_id`    TEXT    NOT NULL,
+  PRIMARY KEY (`business`, `uid`, `id`, `gacha_type`)
+);
+
+INSERT INTO `HG_GACHA_RECORDS`
+  (`business`, `uid`, `id`, `gacha_type`, `gacha_id`,
+  `rank_type`, `count`, `time`, `lang`, `name`, `item_type`, `item_id`)
+SELECT
+  `business`, `uid`, `id`, `gacha_type`, `gacha_id`,
+  `rank_type`, `count`, `time`, `lang`, `name`, `item_type`, `item_id`
+FROM `HG_GACHA_RECORDS_OLD`;
+
+DROP TABLE `HG_GACHA_RECORDS_OLD`;
+
+CREATE INDEX IF NOT EXISTS `HG_GACHA_RECORDS.id_idx`                      ON `HG_GACHA_RECORDS` (`id`);
+CREATE INDEX IF NOT EXISTS `HG_GACHA_RECORDS.gacha_type_idx`              ON `HG_GACHA_RECORDS` (`gacha_type`);
+CREATE INDEX IF NOT EXISTS `HG_GACHA_RECORDS.rank_type_idx`               ON `HG_GACHA_RECORDS` (`rank_type`);
+CREATE INDEX IF NOT EXISTS `HG_GACHA_RECORDS.business_uid_idx`            ON `HG_GACHA_RECORDS` (`business`, `uid`);
+CREATE INDEX IF NOT EXISTS `HG_GACHA_RECORDS.business_uid_gacha_type_idx` ON `HG_GACHA_RECORDS` (`business`, `uid`, `gacha_type`);
+
+PRAGMA USER_VERSION = 2;
+
+RELEASE start_migration_v2;
+COMMIT TRANSACTION;
+";
+
+const SQLS: &[&str] = &[SQL_V1, SQL_V2];
 
 // endregion
 
@@ -533,7 +584,7 @@ impl GachaRecordSaveOnConflict {
         ) VALUES (
           ?, ?, ?, ?, ?, ?,
           ?, ?, ?, ?, ?, ?
-        ) ON CONFLICT (`business`, `uid`, `id`) DO NOTHING;"
+        ) ON CONFLICT (`business`, `uid`, `id`, `gacha_type`) DO NOTHING;"
       }
       Self::Update => {
         "INSERT INTO `HG_GACHA_RECORDS` (
@@ -542,8 +593,7 @@ impl GachaRecordSaveOnConflict {
         ) VALUES (
           ?, ?, ?, ?, ?, ?,
           ?, ?, ?, ?, ?, ?
-        ) ON CONFLICT (`business`, `uid`, `id`) DO UPDATE SET
-          `gacha_type` = excluded.`gacha_type`,
+        ) ON CONFLICT (`business`, `uid`, `id`, `gacha_type`) DO UPDATE SET
           `gacha_id`   = excluded.`gacha_id`,
           `rank_type`  = excluded.`rank_type`,
           `count`      = excluded.`count`,
