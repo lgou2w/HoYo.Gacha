92b0d2c7f2800baf2bf611b1ab01d90604c05ffd
lgou2w <lgou2w@hotmail.com>
2025-08-01 13:42:27 +0800

    fix: allows building without git

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,4 @@
+use std::io;
 use std::process::Command;
 
 fn main() {
@@ -5,25 +6,31 @@ fn main() {
 
   // Embedded Environment Variables
   {
-    let git_commit_hash = String::from_utf8(
-      Command::new("git")
-        .args(["rev-parse", "HEAD"])
-        .output()
-        .unwrap()
-        .stdout,
-    )
-    .unwrap();
+    const NA: &str = "N/A";
 
-    let git_commit_date = String::from_utf8(
-      Command::new("git")
-        .args(["show", "--no-patch", "--format=%ci"])
-        .output()
-        .unwrap()
-        .stdout,
-    )
-    .unwrap();
-
+    // Allows building without git
+    let (git_commit_hash, git_commit_date) = git_commit().unwrap_or((NA.into(), NA.into()));
     println!("cargo:rustc-env=GIT_COMMIT_HASH={git_commit_hash}");
     println!("cargo:rustc-env=GIT_COMMIT_DATE={git_commit_date}");
   }
+}
+
+fn git_commit() -> io::Result<(String, String)> {
+  let hash = String::from_utf8(
+    Command::new("git")
+      .args(["rev-parse", "HEAD"])
+      .output()?
+      .stdout,
+  )
+  .unwrap();
+
+  let date = String::from_utf8(
+    Command::new("git")
+      .args(["show", "--no-patch", "--format=%ci"])
+      .output()?
+      .stdout,
+  )
+  .unwrap();
+
+  Ok((hash, date))
 }
