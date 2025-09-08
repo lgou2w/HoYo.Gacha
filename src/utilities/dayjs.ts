42c6552e16fea13189664c4f1312699116042108
lgou2w <lgou2w@hotmail.com>
2025-09-08 15:18:36 +0800

    fix(#115): Simplified Chinese uses a 24-hour system

--- src/utilities/dayjs.ts
+++ src/utilities/dayjs.ts
@@ -6,10 +6,18 @@ import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import updateLocale from 'dayjs/plugin/updateLocale'
 
 dayjs.extend(isSameOrAfter)
 dayjs.extend(isSameOrBefore)
 dayjs.extend(localizedFormat)
 dayjs.extend(relativeTime)
+dayjs.extend(updateLocale)
+dayjs.updateLocale('zh-cn', {
+  formats: {
+    // YYYY年M月D日ddddAh点mm分 -> 24 hours
+    LLLL: 'YYYY年M月D日dddd HH点mm分',
+  },
+})
 
 export default dayjs
