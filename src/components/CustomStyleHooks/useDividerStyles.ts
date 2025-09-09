b2a983390a414d8b115a49118cfce2dc04c61cb9
lgou2w <lgou2w@hotmail.com>
2025-09-09 18:37:56 +0800

    feat: add custom styles for Divider, TableCell, and TableHeaderCell components

---
+++ src/components/CustomStyleHooks/useDividerStyles.ts
@@ -0,0 +1,26 @@
+import { DividerState, getSlotClassNameProp_unstable, makeStyles, mergeClasses } from '@fluentui/react-components'
+
+// This component reimplemented the Fluent UI rendering in order to override some of the default styles.
+// See:
+//  https://github.com/microsoft/fluentui/blob/master/packages/react-components/react-divider/library/src/components/Divider
+//  https://react.fluentui.dev/?path=/docs/components-divider--docs
+
+const useVerticalStyles = makeStyles({
+  root: {
+    minHeight: '1rem',
+  },
+  withChilder: {
+    minHeight: '5.25rem',
+  },
+})
+
+export default function useDividerStyles (state: DividerState) {
+  const verticalStyles = useVerticalStyles()
+
+  state.root.className = mergeClasses(
+    state.root.className,
+    state.vertical && verticalStyles.root,
+    state.vertical && state.root.children !== undefined && verticalStyles.withChilder,
+    getSlotClassNameProp_unstable(state.root),
+  )
+}
---
+++ src/components/CustomStyleHooks/useTableCellStyles.ts
@@ -0,0 +1,43 @@
+import { TableCellState, getSlotClassNameProp_unstable, makeStyles, mergeClasses } from '@fluentui/react-components'
+
+// This component reimplemented the Fluent UI rendering in order to override some of the default styles.
+// See:
+//  https://github.com/microsoft/fluentui/blob/master/packages/react-components/react-table/library/src/components/TableCell
+//  https://react.fluentui.dev/?path=/docs/components-table--docs
+
+const useTableLayoutStyles = makeStyles({
+  medium: {
+    height: '2.75rem',
+  },
+  small: {
+    height: '2.125rem',
+  },
+  'extra-small': {
+    height: '1.5rem',
+  },
+})
+
+const useFlexLayoutStyles = makeStyles({
+  medium: {
+    minHeight: '2.75rem',
+  },
+  small: {
+    minHeight: '2.125rem',
+  },
+  'extra-small': {
+    minHeight: '1.5rem',
+  },
+})
+
+export default function useTableCellStyles (state: TableCellState) {
+  const layoutStyles = {
+    table: useTableLayoutStyles(),
+    flex: useFlexLayoutStyles(),
+  }
+
+  state.root.className = mergeClasses(
+    state.root.className,
+    state.noNativeElements ? layoutStyles.flex[state.size] : layoutStyles.table[state.size],
+    getSlotClassNameProp_unstable(state.root),
+  )
+}
