b96d8f75a6b15538d2255604fa3482854d9e6916
lgou2w <lgou2w@hotmail.com>
2026-01-26 12:22:02 +0800

    chore: upstream updates of custom style hooks

--- app/contexts/Theme/CustomStyleHooks/useSwitchStyles.ts
+++ app/contexts/Theme/CustomStyleHooks/useSwitchStyles.ts
@@ -7,17 +7,30 @@ import { SwitchState, getSlotClassNameProp_unstable, makeStyles, mergeClasses, s
 //  https://github.com/microsoft/fluentui/tree/master/packages/react-components/react-switch/library/src/components/Switch
 //  https://react.fluentui.dev/?path=/docs/components-switch--docs
 
-const TrackHeight = '1.25rem'
-const TrackWidth = '2.5rem'
-const ThumbSize = '1.125rem'
+const TrackHeightMedium = '1.25rem'
+const TrackWidthMedium = '2.5rem'
+const ThumbSizeMedium = '1.125rem'
+const TrackHeightSmall = '1rem'
+const TrackWidthSmall = '2rem'
+const ThumbSizeSmall = '0.875rem'
 
 const useInputStyles = makeStyles({
-  root: {
-    width: `calc(${TrackWidth} + 2 * ${tokens.spacingHorizontalS})`,
+  medium: {
+    width: `calc(${TrackWidthMedium} + 2 * ${tokens.spacingHorizontalS})`,
     ':checked': {
       [`& ~ .${switchClassNames.indicator}`]: {
         '> *': {
-          transform: `translateX(calc(${TrackWidth} - ${ThumbSize} - 0.125rem))`,
+          transform: `translateX(calc(${TrackWidthMedium} - ${ThumbSizeMedium} - 0.125rem))`,
+        },
+      },
+    },
+  },
+  small: {
+    width: `calc(${TrackWidthSmall} + 2 * ${tokens.spacingHorizontalS})`,
+    ':checked': {
+      [`& ~ .${switchClassNames.indicator}`]: {
+        '> *': {
+          transform: `translateX(calc(${TrackWidthSmall} - ${ThumbSizeSmall} - 0.125rem))`,
         },
       },
     },
@@ -25,17 +38,26 @@ const useInputStyles = makeStyles({
 })
 
 const useLabelStyles = makeStyles({
-  root: {
-    marginBottom: `calc((${TrackHeight} - ${tokens.lineHeightBase300}) / 2)`,
-    marginTop: `calc((${TrackHeight} - ${tokens.lineHeightBase300}) / 2)`,
+  medium: {
+    marginBottom: `calc((${TrackHeightMedium} - ${tokens.lineHeightBase300}) / 2)`,
+    marginTop: `calc((${TrackHeightMedium} - ${tokens.lineHeightBase300}) / 2)`,
+  },
+  small: {
+    marginBottom: `calc((${TrackHeightSmall} - ${tokens.lineHeightBase200}) / 2)`,
+    marginTop: `calc((${TrackHeightSmall} - ${tokens.lineHeightBase200}) / 2)`,
   },
 })
 
 const useIndicatorStyles = makeStyles({
-  root: {
-    fontSize: ThumbSize,
-    height: TrackHeight,
-    width: TrackWidth,
+  medium: {
+    fontSize: ThumbSizeMedium,
+    height: TrackHeightMedium,
+    width: TrackWidthMedium,
+  },
+  small: {
+    fontSize: ThumbSizeSmall,
+    height: TrackHeightSmall,
+    width: TrackWidthSmall,
   },
 })
 
@@ -46,20 +68,20 @@ export default function useSwitchStyles (state: SwitchState) {
 
   state.input.className = mergeClasses(
     state.input.className,
-    inputStyles.root,
+    inputStyles[state.size],
   )
 
   if (state.label) {
     state.label.className = mergeClasses(
       state.label.className,
-      labelStyles.root,
+      labelStyles[state.size],
       getSlotClassNameProp_unstable(state.label),
     )
   }
 
   state.indicator.className = mergeClasses(
     state.indicator.className,
-    indicatorStyles.root,
+    indicatorStyles[state.size],
     getSlotClassNameProp_unstable(state.indicator),
   )
 }
