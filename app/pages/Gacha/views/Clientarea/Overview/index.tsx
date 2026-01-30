82667b98d292c33b61318b597afc8d0a907dc3c7
lgou2w <lgou2w@hotmail.com>
2026-01-30 16:04:45 +0800

    feat: add no account component

--- app/pages/Gacha/views/Clientarea/Overview/index.tsx
+++ app/pages/Gacha/views/Clientarea/Overview/index.tsx
@@ -1,5 +1,6 @@
 import { makeStyles, tokens } from '@fluentui/react-components'
 import ClientareaLastUpdated from '@/pages/Gacha/components/Clientarea/LastUpdated'
+import ClientareaNoAccount from '@/pages/Gacha/components/Clientarea/NoAccount'
 import { usePrettizedRecords } from '@/pages/Gacha/contexts/PrettizedRecords'
 import OverviewGrid from './Grid'
 import OverviewTooltips from './Tooltips'
@@ -15,12 +16,12 @@ const useStyles = makeStyles({
 
 export default function OverviewView () {
   const styles = useStyles()
-  const { data } = usePrettizedRecords()
+  const { selected, data } = usePrettizedRecords()
 
-  // FIXME: This situation usually occurs when no accounts are available.
-  //   Users need to be instructed to create one manually.
-  if (!data) {
-    return null
+  // This situation usually occurs when no accounts are available.
+  // Users need to be instructed to create one manually.
+  if (!selected || !data) {
+    return <ClientareaNoAccount />
   }
 
   return (
