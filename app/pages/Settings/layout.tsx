3bfa9b05688c681bfc91032a9f2649617463c22a
lgou2w <lgou2w@hotmail.com>
2026-02-03 15:31:32 +0800

    feat: improve settings page

--- app/pages/Settings/layout.tsx
+++ app/pages/Settings/layout.tsx
@@ -2,13 +2,26 @@ import { makeStyles, tokens } from '@fluentui/react-components'
 import About from '@/pages/Settings/components/About'
 import Appearance from '@/pages/Settings/components/Appearance'
 import General from '@/pages/Settings/components/General'
+import Hero from '@/pages/Settings/components/Hero'
 import LegacyMigration from '@/pages/Settings/components/LegacyMigration'
 
 const useStyles = makeStyles({
   root: {
     display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    flex: '1 0 auto',
+    gap: tokens.spacingHorizontalL,
+  },
+  sections: {
+    display: 'flex',
     flexDirection: 'column',
-    rowGap: tokens.spacingHorizontalL,
+    flex: '1 0 70%',
+    rowGap: tokens.spacingVerticalL,
+  },
+  hero: {
+    display: 'flex',
+    flex: 1,
   },
 })
 
@@ -17,10 +30,15 @@ export default function SettingsLayout () {
 
   return (
     <div className={styles.root}>
-      <LegacyMigration />
-      <General />
-      <Appearance />
-      <About />
+      <div className={styles.sections}>
+        <LegacyMigration />
+        <General />
+        <Appearance />
+        <About />
+      </div>
+      <div className={styles.hero}>
+        <Hero />
+      </div>
     </div>
   )
 }
