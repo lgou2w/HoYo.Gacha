d783d8ead957a87f375652dce27d2489e43b5ca1
lgou2w <lgou2w@hotmail.com>
2026-02-07 19:13:39 +0800

    feat: complete the gacha clientarea tabs component

--- app/pages/Settings/components/General/index.tsx
+++ app/pages/Settings/components/General/index.tsx
@@ -1,11 +1,13 @@
 import { WithTrans, withTrans } from '@/i18n'
 import SectionGroup from '@/pages/Settings/components/SectionGroup'
+import GachaClientareaTabs from './GachaClientareaTabs'
 import Language from './Language'
 
 export default withTrans.SettingsPage(function General ({ t }: WithTrans) {
   return (
     <SectionGroup title={t('General.Title')}>
       <Language />
+      <GachaClientareaTabs />
     </SectionGroup>
   )
 })
