1e2c7bd9a079ab77c1c77e506ea76a075c4d728d
lgou2w <lgou2w@hotmail.com>
2026-03-15 08:41:15 +0800

    chore: adjust column gap in Metadata and Updater

--- app/pages/Settings/components/About/Metadata.tsx
+++ app/pages/Settings/components/About/Metadata.tsx
@@ -23,7 +23,7 @@ const useActionStyles = makeStyles({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    columnGap: tokens.spacingHorizontalS,
+    columnGap: tokens.spacingHorizontalM,
   },
   statusWrapper: {
     display: 'flex',
